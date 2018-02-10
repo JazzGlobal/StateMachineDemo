25e0f9fa4790d4d08793e51dca40184e699c57ec
Christopher Gambrell <waymer147@gmail.com>
2018-02-10 14:55:36 -0500

    Delete State.java

--- State.java
+++
@@ -1,8 +0,0 @@
-package state;
-
-public abstract class State {
-    public abstract void Init();
-    public abstract void Update();
-    public abstract void onEnter();
-    public abstract void onExit();
-}
